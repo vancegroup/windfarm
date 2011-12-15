276f7306a6857bcd06ed375a93320332905d5a8a
Ryan Pavlik <rpavlik@iastate.edu>
2011-12-15 16:40:16 -0600

    Some work-in-progress lighting from my computer.

--- windfarm.lua
+++ windfarm.lua
@@ -7,28 +7,6 @@ fn = getScriptFilename()
 assert(fn, "Have to load this from file, not copy and paste, or we can't find our models!")
 vrjLua.appendToModelSearchPath(fn)
 
-RelativeTo.Room:addChild(
-	Lighting{
-		number = 0,
-		ambient = 1.0,
-		diffuse = 0.7,
-		specular = 0.5,
-		position = {0, 3, 2},
-		positional = false
-	}
-)
-RelativeTo.Room:addChild(
-	Lighting{
-		number = 1,
-		ambient = 1.0,
-		diffuse = 0.7,
-		specular = 0.5,
-		position = {0, 6, 2},
-		positional = true
-	}
-)
-
-
 farm = --AmbientIntensity{
 	--intensity = 0.8,
 	Group{
@@ -121,4 +99,51 @@ RelativeTo.World:addChild(
 print("Run DebugAxes.show() to show coordinate axes") 
 --DebugAxes.show()
 DebugAxes.hide()
+
+light = {}
+function addLight(arg)
+	local l = osg.Light()
+	l:setLightNum(arg.number)
+	light[arg.number] = l
+	
+	local ls = osg.LightSource()
+	ls:setLight(l)
+	ls:setLocalStateSetModes(osg.StateAttribute.Values.ON)
+	
+	l:setAmbient(osg.Vec4(arg.ambient, arg.ambient, arg.ambient, 1))
+	l:setDiffuse(osg.Vec4(arg.diffuse, arg.diffuse, arg.diffuse, 1))
+	l:setSpecular(osg.Vec4(arg.specular, arg.specular, arg.specular, 1))
+	
+	l:setPosition(arg.position)
+	return ls
+end
+
+lightGroup = osg.Group()
+RelativeTo.Room:addChild(lightGroup)
+ss = RelativeTo.Room:getOrCreateStateSet()
+	local dir = osg.Vec3f(0, -1, 0)
+	dir:normalize()
+	lightGroup:addChild(
+		addLight{
+			number = 0,
+			ambient = 0.8,
+			diffuse = 0.7,
+			specular = 0.0,
+			position = osg.Vec4(0, 3, 2, 1),
+			direction = dir
+		}
+	)
+	
+	ss:setAssociatedModes(light[0], osg.StateAttribute.Values.ON)
+	lightGroup:addChild(
+		addLight{
+			number = 1,
+			ambient = 0.0,
+			diffuse = 0.5,
+			specular = 0.1,
+			position = osg.Vec4(0, 2, 2, 0),
+			direction = osg.Vec3f(0, -1, 0)
+		}
+	)
+	ss:setAssociatedModes(light[1], osg.StateAttribute.Values.ON)
 
